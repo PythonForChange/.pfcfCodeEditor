bfa86a257f7313eb0ab151fbdc122c51b6d545ec
Emmanuel Norambuena <38821970+eanorambuena@users.noreply.github.com>
2021-06-13 22:36:50 +0000

    1p0.1 Bug solved: "no leía el código en python, qiskit ni wolfram"

--- Pyfoch.py
+++ Pyfoch.py
@@ -108,6 +108,186 @@ class Parser():
   def isDeny(self,x: str):
     return self.compare(x,self.den)
 
+def codef(codel: str,text: str): #code function
+  if codel=="qiskit":
+    t=qiskit(text)
+  elif codel=="wolfram":
+    t=wolfram(text)
+  elif codel=="python":
+    t=python(text)
+  return t
+
+def qiskit(text: str):
+  T=""
+  T+="from qiskit import QuantumCircuit, execute, Aer\n"
+  T+="from qiskit.visualization import plot_histogram,display\n"
+  s=0
+  command=""
+  param=""
+  Q=0
+  gate=""
+  gatecount=0
+  qdef=0
+  for i in text:
+    if i==",":
+      pass
+    elif s==1: #settings mode on
+      if i!=" ":
+        command+=i
+      else:
+        s=2
+    elif s==2:
+      if i!=" " and i!="\n":
+        param+=i
+      else:
+        T+=qsettings(command,param)
+        command=""
+        param=""
+        s=0
+    elif i=="$":
+      s=1
+    elif qdef==1:
+      if i=="q":
+        Q+=1
+      elif i=="\n":
+        qdef=2
+        T+="circuit=QuantumCircuit("+str(Q)+","+str(Q)+")\n"
+    elif qdef==2:
+      if i!="\n" and i!=" ":
+        gate+=i
+      elif i==" ":
+        gatecount+=0.5
+        T+=quantum(gate,gatecount)
+        gate=""
+      else:
+        T+=quantum(gate,gatecount)
+        gate=""
+        gatecount=0
+    elif i=="q":
+      qdef=1
+      Q+=1
+  return T
+
+def wolfram(text: str):
+  T=""
+  T+="from wolframclient.evaluation import WolframLanguageSession\n"
+  T+="from wolframclient.language import wl, wlexpr\n"
+  T+="session = WolframLanguageSession()\n"
+  s=0
+  command=""
+  param=""
+  t=""
+  for i in text:
+    if i==",":
+      pass
+    elif s==1: #settings mode on
+      if i!=" ":
+        command+=i
+      else:
+        s=2
+    elif s==2:
+      if i!=" " and i!="\n":
+        param+=i
+      else:
+        T+=wsettings(command,param)
+        command=""
+        param=""
+        s=0
+    elif i=="$":
+      s=1
+    elif i=="\n" and t!="":
+      T+="session.evaluate(wlexpr(\'"+t+"\'))\n"
+      t=""
+    elif i!="\n":
+      t+=i
+  return T
+
+def python(text: str):
+  T=""
+  T+="from pyforchange.pfcf.utils import *\n"
+  T+="from pyforchange.pfcf.read import *\n"
+  s=0
+  command=""
+  param=""
+  for i in text:
+    if i==",":
+      pass
+    elif s==1: #settings mode on
+      if i!=" ":
+        command+=i
+      else:
+        s=2
+    elif s==2:
+      if i!=" " and i!="\n":
+        param+=i
+      else:
+        T+=psettings(command,param)
+        command=""
+        param=""
+        s=0
+    elif i=="$":
+      s=1
+    else:
+      T+=i
+  return T
+
+def psettings(command: str,param):
+  t=""
+  if command=="pfcf":
+    t="executepfcf(\""+param+"\")"
+  elif command=="python":
+    t="execute(\""+param+"\")"
+  return t
+
+def wsettings(command: str,param):
+  t=""
+  if command=="inject":
+    t=param+"\n"
+  return t
+
+def qsettings(command: str,param):
+  t=""
+  if command=="host":
+    t0="s=1024\nbackend=Aer.get_backend('"+param+"')\n"
+    t1="job=execute(circuit, backend, shots=s)\n"
+    t2="result=job.result()\n"
+    t3="counts=result.get_counts(circuit)\n"
+    t=t0+t1+t2+t3
+  elif command=="shots":
+    t="s="+param
+  elif command=="hist":
+    t1="graph=plot_histogram(counts)\n"
+    t2="display(graph)\n"
+    t=t1+t2
+  elif command=="draw":
+    t="circuit.draw('mpl')\n"
+  elif command=="inject":
+    t=param+"\n"
+  return t
+
+def quantum(gate: str,n):
+  t=""
+  N=floor(n)
+  n=str(N)
+  T="circuit."
+  if len(gate)==1:
+    t=T+gate.lower()+"("+n+")\n"
+  elif gate==".---X":
+    t=T+"cx("+n+","+str(N+1)+")\n"
+  elif gate=="X---.":
+    t=T+"cx("+str(N+1)+","+n+")\n"
+  else:
+    number=""
+    b=0
+    for i in gate:
+      if i!="c":
+        number+=i
+      else:
+        b=1
+    if b==1:
+      t=T+"measure("+n+","+number+")\n"
+  return t
+
 def pfcfread(name: str,printYesOrNo: int =1,returnText: int =0):
   lines=getLines(name+".pfcf")
   T=""
@@ -138,7 +318,7 @@ def pfcfread(name: str,printYesOrNo: int =1,returnText: int =0):
         codem+=1
       elif codem==4:
         try:
-          codef(codel,code)
+          T+=codef(codel,code)
           codem==0
           codel=""
           code=""
@@ -191,6 +371,7 @@ def export():
 
 #Tools Menu
 filemenu.add_command(label="Export", command=export)
+
 '''
 This code is based on the code from 
 https://docs.hektorprofe.net/python/interfaces-graficas-con-tkinter/editor-de-texto
